16f6288f04a1c6ac7e39ae579706dcb771bc67da
Alisson Henrique <alisson-henri@live.com>
2020-10-12 19:40:16 -0300

    authenticate

--- BackEnd/src/shared/container/index.ts
+++ BackEnd/src/shared/container/index.ts
@@ -3,13 +3,13 @@ import '@modules/users/providers';
 // import './providers';
 import IUserRepository from '@modules/users/repositories/IUserRepository';
 import UserRepository from '@modules/users/infra/typeorm/repositories/UserRepository';
-import IUsersTokenRepository from '@modules/users/repositories/IUserTokenRepository';
+// import IUsersTokenRepository from '@modules/users/repositories/IUserTokenRepository';
 
-import UsersTokenRepository from '@modules/users/infra/typeorm/repositories/UsersTokenRepository';
+// import UsersTokenRepository from '@modules/users/infra/typeorm/repositories/UsersTokenRepository';
 
 container.registerSingleton<IUserRepository>('UserRepository', UserRepository);
 
-container.registerSingleton<IUsersTokenRepository>(
-  'UsersTokenRepository',
-  UsersTokenRepository,
-);
+// container.registerSingleton<IUsersTokenRepository>(
+//   'UsersTokenRepository',
+//   UsersTokenRepository,
+// );
